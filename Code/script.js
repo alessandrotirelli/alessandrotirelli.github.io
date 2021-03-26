424f2be99bbb635a6162878d653400863a621c13
Alessandro Tirelli <alessandrtirelli02@gmail.com>
2021-03-26 16:45:33 +0100

    2020.03.26_v1

--- Code/script.js
+++ Code/script.js
@@ -14,13 +14,13 @@ $(function () {
 $(document).ready(function () {
   // executes when HTML-Document is loaded and DOM is ready
 
-    $(".card-group").hover(
+    $(".card").hover(
         function () {
-            $(this).find(".card").addClass("shadow-lg").css("transition", ".3s");
+            $(this).addClass("shadow-lg").css("transition", ".3s");
             //$(this).addClass("shadow").css({"cursor": "pointer", "transition": ".3s", "transform": "scale(1.1)"});
         },
         function () {
-            $(this).find(".card").removeClass("shadow-lg");
+            $(this).removeClass("shadow-lg");
             //$(this).removeClass("shadow").css("transform", "scale(1)");
         }
     );
